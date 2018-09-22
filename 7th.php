d4f4fa83accb637cbfe3504e8b88f0d91e376dfa
Puneeth N <42162624+TechCodeDev@users.noreply.github.com>
2018-09-22 21:26:39 +0530

    Add files via upload

---
+++ 7th.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+<meta http-equiv="refresh" content="1"/>
+<style>
+p {
+color:white;
+font-size:90px;
+position: absolute;
+top: 50%;
+left: 50%;
+transform: translate(-50%, -50%);
+}
+body {background-color: yellow;}
+</style>
+<p> <?php echo date(" h: i : s A");?> </p>
+</head>
